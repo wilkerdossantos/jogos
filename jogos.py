06e5c78bffabe435db7819c7fd6b8bc9350ae14c
Wilker Santos <78004450+wilkerdossantos@users.noreply.github.com>
2021-01-30 00:27:48 -0300

    Update jogos
    Adicionado laço e função para clear no terminal.

--- jogos.py
+++ jogos.py
@@ -1,20 +1,25 @@
 import forca
 import adivinhacao
+import os
 
 def escolhe_jogo():
-    print("*********************************")
-    print("*******Escolha o seu jogo!*******")
-    print("*********************************")
+    while(True):
+        os.system('clear') or None
+        print("*********************************")
+        print("*******Escolha o seu jogo!*******")
+        print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+        print("(1) Forca (2) Adivinhação")
 
-    jogo = int(input("Qual jogo?"))
+        jogo = int(input("Qual jogo?"))
 
-    if(jogo == 1):
-        print("Jogando Forca")
-        forca.jogar()
-    elif(jogo == 2):
-        print("Jogando Adivinhação")
-        adivinhacao.jogar()
+        if(jogo == 1):
+            print("Jogando Forca")
+            forca.jogar()
+        elif(jogo == 2):
+            print("Jogando Adivinhação")
+            adivinhacao.jogar()
+        print("Pressione Enter para continuar ou Ctrl+Z para encerrar....")
+        input()
 if(__name__ == "__main__"):
     escolhe_jogo()
